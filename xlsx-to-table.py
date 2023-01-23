f04384a044b81fbdc2cb5d9a47305508c9572636
Veivel <25278964+Veivel@users.noreply.github.com>
2023-01-23 18:22:52 +0700

    feat: computation question, & compatibility for more dynamic queries

--- xlsx-to-table.py
+++ xlsx-to-table.py
@@ -5,76 +5,104 @@ import pandas as pd
 import os
 from pathlib import Path
 
+# formatnya presetName: [question_table_schema, ans_table_schema]
 preset_schemas = {
-    'countries': '''Country VARCHAR(128), Population INT''',
-    'employees': '''FirstName VARCHAR(64),LastName VARCHAR(64),Salary INT,Department VARCHAR(128)'''
+    'countries' : (
+        '''Country VARCHAR(128), Population INT''', 
+        '''Country VARCHAR(128), Population INT'''
+    ),
+    'employees' : (
+        '''FirstName VARCHAR(64), LastName VARCHAR(64),Salary INT,Department VARCHAR(128)''',
+        '''FirstName VARCHAR(64), LastName VARCHAR(64),Salary INT,Department VARCHAR(128)'''
+    ),
+    'computer'  : (
+        '''ComputerName VARCHAR(128), CostPerHour FLOAT(8), HoursToCompute FLOAT(8)''',
+        '''ComputerName VARCHAR(128), TotalCost FLOAT(8)''',
+    ),
 }
-
 
 #### VARIABLES ####
 
 db_name = "das_test_db"
-db_user = "test"
+db_user = "veii"
 db_password = "" # no need
 delim = ';'
 
-target_xlsx = input("-> path of target XLSX file: ")
-schema = ""
-schema_name = input("-> preset schema (empty if none):")
-if schema_name == "":
-    schema = input("-> raw table schema: ")
-else:
-    schema = preset_schemas[schema_name]
-DROP_FIRST = True
-
-###################
-
-print("-> CSV delimiter: ", delim)
-print("-> Table schema: ", schema)
-proceed = input("Do you want to continue? (Y/n) ")
-
-if proceed.lower() != "y":
-    exit(0)
+def main():
+    # Get Input
+    target_xlsx = input("-> path of target XLSX file: ")
+    schema = ()
+    print("presets available:", preset_schemas.keys())
+    schema_name = input("-> preset schema (empty if none): ")
+    if schema_name == "":
+        schema[0] = input("-> question table schema: ")
+        schema[1] = input("-> answer table schema: ")
+    else:
+        schema = preset_schemas[schema_name]
+    DROP_FIRST = True
     
-psql_config = f"psql -U {db_user} -d {db_name} -c"
+    # Confirmation
+    print("CSV delimiter:", delim)
+    print("Table schema:", schema)
+    proceed = input("Do you want to continue? (Y/n): ")
 
-df_arr = pd.read_excel(target_xlsx, None)
-for sheet_name in df_arr:
-    df = df_arr[sheet_name]
-    
-    print(f"\n======== ======== {sheet_name} ======== ========")
-    print(df)
-    
-    # drop the table ()
-    if DROP_FIRST:
+    if proceed.lower() != "y":
+        exit(0)
+        
+    # Setup Postgres CLI config
+    psql_config = ""
+    if db_password == "":
+        psql_config = f"psql -U {db_user} -d {db_name} -c"
+    else:
+        psql_config = f"psql -U {db_user} -p {db_password} -d {db_name} -c"
+
+    df_arr = pd.read_excel(target_xlsx, None)
+    for sheet_name in df_arr:
+        df = df_arr[sheet_name]
+        df.dropna(axis=0, inplace=True)
+        
+        print(f"\n======== ======== {sheet_name} ======== ========")
+        print(df)
+        
+        # drop the table
+        if DROP_FIRST:
+            print(subprocess.Popen(
+                    f'{psql_config} "DROP TABLE {sheet_name};"', 
+                    stdout=subprocess.PIPE, 
+                    shell=True, 
+                    universal_newlines=True
+                ).stdout.read()
+            )
+        
+        # select schema
+        if "_ans" in sheet_name:
+            curr_schema = schema[1]
+        else:
+            curr_schema = schema[0]
+        
+        # create the table
         print(subprocess.Popen(
-                f'{psql_config} "DROP TABLE {sheet_name};"', 
+                f'{psql_config} "CREATE TABLE {sheet_name} ({curr_schema});"', 
                 stdout=subprocess.PIPE, 
                 shell=True, 
                 universal_newlines=True
             ).stdout.read()
         )
-    
-    # create the table
-    print(subprocess.Popen(
-            f'{psql_config} "CREATE TABLE {sheet_name} ({schema});"', 
-            stdout=subprocess.PIPE, 
-            shell=True, 
-            universal_newlines=True
-        ).stdout.read()
-    )
-    
-    # export sheet to temporary csv
-    relative = Path(".")
-    absolute = os.path.abspath(relative)  # absolute is a str object
-    temp_csv_filename = f"temp_{sheet_name}"
-    df.to_csv(f"{temp_csv_filename}.csv", sep=delim, index=False)
-    
-    # import csv into newly-created table
-    print(subprocess.Popen(
-            f'''{psql_config} "copy {sheet_name} from '{absolute}/{temp_csv_filename}.csv' delimiter '{delim}' csv header;"''', 
-            stdout=subprocess.PIPE, 
-            shell=True, 
-            universal_newlines=True
-        ).stdout.read()
-    )
+        
+        # export sheet to temporary csv
+        relative = Path(".")
+        absolute = os.path.abspath(relative)  # absolute is a str object
+        temp_csv_filename = f"temp/tmp_{sheet_name}"
+        df.to_csv(f"{temp_csv_filename}.csv", sep=delim, index=False)
+        
+        # import csv into newly-created table
+        print(subprocess.Popen(
+                f'''{psql_config} "copy {sheet_name} from '{absolute}/{temp_csv_filename}.csv' delimiter '{delim}' csv header;"''', 
+                stdout=subprocess.PIPE, 
+                shell=True, 
+                universal_newlines=True
+            ).stdout.read()
+        )
+        
+if __name__ == "__main__":
+    main()
